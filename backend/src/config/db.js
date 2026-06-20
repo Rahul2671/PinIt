f669b39df7227123f467d2d3456780418209d732
Rahul2671 <rahulsubha1983@gmail.com>
2026-06-20 16:31:41 +0530

    fix duplicate pool declaration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const { Pool } = require("pg");
-require("dotenv").config();
-
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
