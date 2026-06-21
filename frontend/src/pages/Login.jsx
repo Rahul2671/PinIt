211d5590247073b80deaa2a3b711315ff021b664
Rahul <rahulsubha1983@gmail.com>
2026-06-21 18:00:17 +0530

    Fixed redirecting issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
       );
 
       localStorage.setItem("token", response.data.token);
-      window.location.reload();
+      
       navigate("/");
     } catch (error) {
       console.log(error);
